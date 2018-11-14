7b049ab9fe11edfba7cf112022330cd71b6b256a
Daniel Coetzee <Daniel.Coetzee@Euromonitor.com>
2018-11-14 15:50:17 +0200

    changes

--- grid-functions.js
+++ grid-functions.js
@@ -1,59 +1,99 @@
-let c = document.getElementById("myCanvas");
-let ctx = c.getContext("2d");
-window.onload = function() {
+
+window.onload = function () {
     initialise();
 };
 
+let c;
+let ctx;
 let running;
 let grid;
 let isInitialTick = true;
 let canvasDimension = 500;
 let cellDimensions = 10;
-let cellsPerRow = canvasDimension/cellDimensions;
-let numberOfGridCells = cellsPerRow*cellsPerRow;
+let cellsPerRow = canvasDimension / cellDimensions;
+let numberOfGridCells = cellsPerRow * cellsPerRow;
 
-function initialise(){
+function initialise() {
+    c = document.getElementById("myCanvas");
+    ctx = c.getContext("2d");
     grid = createGrid(numberOfGridCells);
     setGridStartTemplate(grid);
+    
     console.log("OnLoad");
 }
 
-function start(){
-    running = setInterval(function(){populateGrid();}, 300);
-    console.log("Started");
-} 
+function start() {
 
-function stop(){
+    if (isInitialTick) {
+        initialise();
+        
+        console.log("InitTickInit");        
+    }
+    
+    //running = setInterval(function(){populateGrid();}, 300);
+    console.log("Started");
+    populateGrid();
+    isInitialTick = false;
+}
+
+function stop() {
     clearInterval(running);
     console.log("Stopped");
 }
 
-function populateGrid(){
+function clearGrid() {
     if(grid != null){
+        ctx.clearRect(0, 0, c.width, c.height);
+    }
 
-        if(!isInitialTick){ // refactor check temp solution
-            grid.forEach(cell => {
-                determineFuture(cell);
-            });
+    grid = null;     
+    c = null;
+    ctx = null;
+    isInitialTick = true;
+
+    console.log("Cleared");
+}
+
+function populateGrid() {
+    if (grid != null) {
+
+        // if (!isInitialTick) { // refactor check temp solution
+        //     grid.forEach(cell => {
+        //         determineFuture(cell);
+        //         console.log("SecondTick");
+        //     });
+        // }
+        
+        if (!isInitialTick) { // refactor check temp solution
+            determineFuture(grid[1225]);
+            determineFuture(grid[1274]);
+            determineFuture(grid[1275]);
+            determineFuture(grid[1276]);
+            determineFuture(grid[1375]);
+            //grid[1225][2] = true;
+
+            //console.log(grid[1375][2]);
+            
+            console.log("SecondTick");
         }
+        //isInitialTick = false;
 
-        isInitialTick = false;
+        //clearGrid();
 
         grid.forEach(cell => {
             drawCell(cell);
         });
+        console.log(grid[1275][2]);
         console.log("Populate");
-    }
-    else{
+    } else {
         console.log("Bad Grid");
     }
 }
 
-function createGrid(numberOfGridCells){
+function createGrid(numberOfGridCells) {
     let grid = new Array(numberOfGridCells);
     let x = 0;
     let y = 0;
-    //let cellLiveState = false;
 
     for (var i = 0; i < grid.length; i++) {
         grid[i] = new Array(4);
@@ -64,72 +104,80 @@ function createGrid(numberOfGridCells){
 
         x += cellDimensions;
 
-        if(x == canvasDimension){
+        if (x == canvasDimension) {
             x = 0;
-            y += cellDimensions;           
+            y += cellDimensions;
         }
     }
 
     return grid;
 }
 
-function drawCell(cell){
-    if(cell[2]){   
-        ctx.beginPath();
-        ctx.rect(cell[0],cell[1],cellDimensions,cellDimensions);
+function drawCell(cell) {
+    if (cell[2]) {
+        ctx.rect(cell[0], cell[1], cellDimensions, cellDimensions);
         ctx.fill();
     }
 }
 
-function determineFuture(cell){
+function determineFuture(cell) {
 
-    let neighbourImageData = new Array();
-    
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
-    neighbourImageData.push(ctx.getImageData(cell[3][0]+cellDimensions,cell[3][1], canvasDimension, canvasDimension).data); // north neighbour
+    let neighbourImageData = getNeighbourImageData(cell);
 
     let numberOfAliveNeighbors = getNumberOfAliveNeighbors(neighbourImageData);
 
-    if(numberOfAliveNeighbors == 3){
+    console.log(neighbourImageData);
+    console.log(numberOfAliveNeighbors);
+
+    if (numberOfAliveNeighbors == 3) {
         cell[2] = true;
-    }
-    else if(numberOfAliveNeighbors < 3){
+    } else if (numberOfAliveNeighbors < 3) {
         cell[2] = false;
-    }
-    else if(numberOfAliveNeighbors > 3){
+    } else if (numberOfAliveNeighbors > 3) {
         cell[2] = false;
     }
 }
 
-function getNumberOfAliveNeighbors(imgDataArray){
+function getNumberOfAliveNeighbors(imgDataArray) {
     let numberOfAliveNeighbors = 0;
-    for(let i=0;imgDataArray.length<0;i++){
 
-    }
+    imgDataArray.forEach(data => {
+        if (data[3] > 0) { // update to check rgb values
+            numberOfAliveNeighbors++
+        }
+    });
+
+    return numberOfAliveNeighbors;
 }
 
-function clearGrid(){
-    ctx.clearRect(0, 0, c.width, c.height); 
+function getNeighbourImageData(cell){
+    let neighbourImageData = new Array();
+
+    neighbourImageData.push(ctx.getImageData(cell[3][0], cell[3][1] + cellDimensions, 1, 1).data); // north neighbour
+    neighbourImageData.push(ctx.getImageData(cell[3][0], cell[3][1] - cellDimensions, 1, 1).data); // south neighbour
+    neighbourImageData.push(ctx.getImageData(cell[3][0] - cellDimensions, cell[3][1], 1, 1).data); // west neighbour
+    neighbourImageData.push(ctx.getImageData(cell[3][0] + cellDimensions, cell[3][1], 1, 1).data); // east neighbour
+
+    neighbourImageData.push(ctx.getImageData(cell[3][0] - cellDimensions, cell[3][1] + cellDimensions, 1, 1).data); // north-west neighbour
+    neighbourImageData.push(ctx.getImageData(cell[3][0] + cellDimensions, cell[3][1] + cellDimensions, 1, 1).data); // north-east neighbour
+    neighbourImageData.push(ctx.getImageData(cell[3][0] - cellDimensions, cell[3][1] - cellDimensions, 1, 1).data); // south-west neighbour
+    neighbourImageData.push(ctx.getImageData(cell[3][0] + cellDimensions, cell[3][1] - cellDimensions, 1, 1).data); // south-east neighbour
+
+    return neighbourImageData;
 }
 
-function setGridStartTemplate(grid){
+function setGridStartTemplate(grid) {
     grid[1225][2] = true;
     grid[1274][2] = true;
-    grid[1275][2] = true;
+    grid[1275][2] = false;
     grid[1276][2] = true;
     grid[1375][2] = true;
 }
 
-function assignCellCenterCoordinates(cell){
+function assignCellCenterCoordinates(cell) {
     cell[3] = new Array(2);
-    cell[3][0] = (cell[0] + (cellDimensions/2)); // cellCenterCoordinates(x)
-    cell[3][1] = (cell[1] + (cellDimensions/2)); // cellCenterCoordinates(y)
+    cell[3][0] = (cell[0] + (cellDimensions / 2)); // cellCenterCoordinates(x)
+    cell[3][1] = (cell[1] + (cellDimensions / 2)); // cellCenterCoordinates(y)
 }
 
 // function getRandomInt(max) {
@@ -147,9 +195,9 @@ function assignCellCenterCoordinates(cell){
 // function drawRandomLines() {
 //     let c = document.getElementById("myCanvas");
 //     let ctx = c.getContext("2d");
-    
+
 //     for(let i =1; i < 10; i++){
-        
+
 //         ctx.beginPath();
 //         ctx.moveTo(getRandomInt(canvasDimension), getRandomInt(canvasDimension));
 //         ctx.lineTo(getRandomInt(canvasDimension), getRandomInt(canvasDimension));
